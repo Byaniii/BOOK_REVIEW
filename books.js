a02f88a93576a5cf6620911f5d323547dd23e96a
byaniii <vivalph4@gmail.com>
2025-08-23 16:38:37 +0800

    Added the functionality to circulate through books in js file

---
+++ books.js
@@ -0,0 +1,31 @@
+
+const BOOKS = {
+  "no-longer-human": {
+    title: "No Longer Human",
+    author: "Osamu Dazai",
+    cover: "book_covers/human.jpg",          
+    review: "Alienation and identity told through confessional notebooks."
+  },
+  
+  "Meditations": {
+    title: "Meditations",
+    author: "Marcus Aurelius",
+    cover: "book_covers/meditations.jpg",
+    review: "idk fucking know cuh"
+  },
+
+  "The_Hidden_Habits_of_Genius": {
+    title: "The Hidden Habits of Genius",
+    author: "Craig Wright, PH.D",
+    cover: "book_covers/genius.jpg",
+    review: "lorem ipsum",
+  },
+
+  "The_Stanger":{
+    title: "The Stranger",
+    author: "Albert Camus",
+    cover: "book_covers/the_stranger.jpg",
+    review: "lorem ipsum",
+  },
+};
+
