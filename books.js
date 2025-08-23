2430cf6902fbbf38e59586fed69f7e7cbd1a7cf2
byaniii <vivalph4@gmail.com>
2025-08-23 20:44:15 +0800

    added more of the body :)

--- books.js
+++ books.js
@@ -25,7 +25,31 @@ const BOOKS = {
     title: "The Stranger",
     author: "Albert Camus",
     cover: "book_covers/the_stranger.jpg",
-    review: "lorem ipsum",
+    review: `"The stranger" was the first book I've read out of spite. The synopsis of the book follows the story of 
+    Meursault, a peculiar man who represents indifference in emotions. In the first chapter, Meursault finds out about 
+    the death of his mother who lived apart from him in which he takes the time off work in order to visit her, as 
+    I try to recall, a retirement home. Forgive me for it has been about 2 years since I've read this book and it's only now that
+    I've acted upon my thoughts on it. Anyways, Meursault frequents the habit of smoking and drinking coffee. The first peculiar thing you
+    notice about the personality of this protagonist is the first line of the novel, "Maman died today. Or Yesterday maybe, I don't know". 
+    When learning about the death of a loved one, usually, you are met with a plethora of emotions, depending on your past experience
+    with that person. Usually however, it's either of guilt, grief, empathy, and the worst of all, regret. But Meursault, expressed none. 
+    It seemed that he was more concerned of having to ask for a leave of absence at work, wondering which mode of transportation he needed to take
+    in order to arrive at Maman's retirement home. It  reminded me of the novel "Metamorphosis" by Franz Kafka, in which the novel character, Gregor Samsa, woke up one morning to find
+    himself transformed into a giant insect and despite this dilemma, his first thought was how it'll convene him resulting to tardiness at work.
+    There are novels out there such as these that embody an entire dilemma, an entire personality into one person, showcasing to the world 
+    the nuances of experiencing each emotion, each personality. I find myself to have fallen inlove with this genre of novels. Meursault exhibits
+    what we consider as a "nonchalant" person, a person that exhibits no emotions or a certain stillness despite the situation. I think in the recent year, our generation has glorified
+    such nonchalant behaviors, may it be in our romantic partners, or glorified to be embedded in our personality; even seen through a satire angle. 
+    However, this level of "nonchalant" that Meursault exhibits didn't seem as what the average person would show. The personality of this character
+    made it seem that he was incapable of experiencing emotion. Within the story, not only does he experience the death of his mother but later within
+    the story, despite the caveats of his behavior, his finds himself guilty of murder. And, as you may expect, exhibits no form of remorse. Nothing 
+    but plain rationalism that he did what he must. He sulked at the idea of imprisonment of course, as any person would. But, we as individuals, the vessels of our soul
+    we experience the spectrum of emotions on a day to day basis, influenced by objects such as ethics and morality; no matter how persistent we are to deny, 
+    we are humans, therefore, we are bound by emotions, decisions made from emotions. You may claim that you are pragmatic, but isn't it ego that constitute 
+    your need to make such a claim? What then does life look like if none of you decisions are bound by emotions. That is the question that intrigues 
+    the reader of this novel
+    
+    `,
   },
 };
 
